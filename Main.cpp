446cffd54b4060aa3d164729cce61ab5f0f73fa4
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-05-01 11:47:30 -0500

    Planning

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <limits>
 
+//psi: PolyStatic Interpreter
+//"psi" -> interactive interpreter
+//"psi liquify file.ps[liquid,solid]" -> interactive interpreter starting with environment from file.psliquid or file.pssolid
+//"psi solidify file.psliquid" -> compile file.psliquid into file.pssolid
+//"psi melt file.pssolid" -> extract interface information from file.pssolid to file.psgas
+
 int main(int nargs, char const *const *args)
 {
 	for(unsigned i = 0; i < nargs; ++i)
