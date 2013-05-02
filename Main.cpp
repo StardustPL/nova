b2b721ce0c089a653be0d52ff85db26f03248cb9
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-05-02 09:32:05 -0500

    More planning

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
 #include <limits>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 //psi: PolyStatic Interpreter
 //"psi" -> interactive interpreter
-//"psi liquify file.ps[liquid,solid]" -> interactive interpreter starting with environment from file.psliquid or file.pssolid
-//"psi solidify file.psliquid" -> compile file.psliquid into file.pssolid
-//"psi melt file.pssolid" -> extract interface information from file.pssolid to file.psgas
+//"psi liquify file.ps[liquid,solid]..." -> interactive interpreter starting with environment from file.psliquid or file.pssolid (*.ps*id)
+//"psi solidify file.psliquid..." -> compile file.psliquid into file.pssolid
+//"psi melt file.pssolid..." -> extract interface information from file.pssolid to file.psgas
 
-int main(int nargs, char const *const *args)
+void Liquify(std::vector<std::string> const &files = std::vector<std::string>());
+void Solidify(std::vector<std::string> const &files);
+void Melt(std::vector<std::string> const &files);
+
+int main(int nargs, char const *const *args_)
 {
-	for(unsigned i = 0; i < nargs; ++i)
+//	auto &WaitForEnter = []{ std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); };
+	if(nargs < 1) return -1;
+	std::vector<std::string> args;
+	std::for_each(args_+1, args_+nargs, [&args](char const *s)
 	{
-		std::cout << args[i] << std::endl;
-	}
+		args.push_back(s);
+	});
 
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	if(args.size() == 0)
+	{
+		Liquify();
+	}
+	else
+	{
+		std::string command = args[0];
+		args.pop_front();
+		std::transform(command.begin(), command.end(), command.begin(), ::tolower);
+		if(command == "liquify")
+		{
+			Liquify(args);
+		}
+		else if(command == "solidify")
+		{
+			Solidify(args);
+		}
+		else if(command == "melt")
+		{
+			Melt(args);
+		}
+		else
+		{
+			std::cout << '"' << command << "\" is not a command" << std::endl;
+		}
+	}
 }
